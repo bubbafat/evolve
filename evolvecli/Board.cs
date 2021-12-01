ce678121f1485e917f2b62a8767fe5205cb2ac3d
Robert Horvick <robert.horvick@gmail.com>
2021-12-01 16:58:32 -0500

    Node coloring is back and moving render.sh into the cli project

--- evolvecli/Board.cs
+++ evolvecli/Board.cs
@@ -11,7 +11,7 @@ namespace evolvecli
         private readonly SKSurface _surface;
         private readonly SKCanvas _canvas;
         private readonly World _world;
-        private readonly Dictionary<int, SKPaint> _colorCache;
+        private readonly Dictionary<int, SKPaint> _paintCache;
         private readonly SKPaint _zoneColor;
         
         private const int CellMultiple = 9;
@@ -24,20 +24,35 @@ namespace evolvecli
             var info = new SKImageInfo(_world.Dimension * CellMultiple, _world.Dimension * CellMultiple);
             _surface = SKSurface.Create(info);
             _canvas = _surface.Canvas;
-            _colorCache = new Dictionary<int, SKPaint>();
+            _paintCache = new Dictionary<int, SKPaint>();
             _zoneColor = new SKPaint
             {
                 Color = SKColors.Red
             };
         }
-
+        
         private void renderNode(Node n)
         {
             SKPoint point = new SKPoint(
                 n.Location.X * CellMultiple + NodeRadius,
                 n.Location.Y * CellMultiple + NodeRadius);
+
+            int paintKey = n.GetHashCode();
+            if (!_paintCache.TryGetValue(paintKey, out SKPaint paint))
+            {
+                var bytes = BitConverter.GetBytes(n.GetHashCode());
+                SKColor color = new SKColor(bytes[0], bytes[1], bytes[2]);
+
+                paint = new SKPaint
+                {
+                    Color = color,
+                    Style = SKPaintStyle.Fill
+                };
+
+                _paintCache.Add(paintKey, paint);
+            }
             
-            _canvas.DrawCircle(point, NodeRadius, _zoneColor);
+            _canvas.DrawCircle(point, NodeRadius, paint);
         }
 
         private void renderFrame()
