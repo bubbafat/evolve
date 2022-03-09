b98f9c16a3220caaddc737c650c5bfbaae38f9e8
Robert Horvick <Robert.Horvick@citrix.com>
2022-03-09 16:03:54 -0500

    Resetting to left side breeding and default iterations

--- evolve/Simulation.cs
+++ evolve/Simulation.cs
@@ -5,12 +5,12 @@ namespace evolve
     public static class Simulation
     {
         public const int TotalNodes = 1000;
-        public const int Generations = 5000;
+        public const int Generations = 1000;
         public const int StepsPerGeneration = FPS * 10;
         public const double SuccessThreshold = 0.98;
         public const int BoardDimensions = 128;
-        public const int GenesPerGenome = 8;
-        public const int InnerNeurons = 3;
+        public const int GenesPerGenome = 2;
+        public const int InnerNeurons = 1;
         public const double MutationChance = 1d / TotalNodes; 
         public const bool AllowKillers = false;
         public const bool AllowBullies = true;
@@ -43,12 +43,15 @@ namespace evolve
 
         public static bool InBreedingGrounds(int x, int y)
         {
+            // left side
+            return x < 15;
+            
             // left side but not the corners
             // return x < 15 && y > 15 && y < BoardDimensions - 15;
             
             // the middle
-            return x > BoardDimensions / 3 && x < (BoardDimensions / 3) * 2
-                                           && y > BoardDimensions / 3 && y < (BoardDimensions / 3) * 2;
+            // return x > BoardDimensions / 3 && x < (BoardDimensions / 3) * 2
+            //                               && y > BoardDimensions / 3 && y < (BoardDimensions / 3) * 2;
         }
     }
 }
