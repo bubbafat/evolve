83ac25a4a59c06352c1745dcc98780cfe128d9bf
Robert Horvick <Robert.Horvick@citrix.com>
2022-03-10 14:39:30 -0500

    update before switching machines

--- evolvecli/Program.cs
+++ evolvecli/Program.cs
@@ -9,7 +9,7 @@ namespace evolvecli
             World world = new World(Simulation.BoardDimensions);
 
             // middle vert
-            //world.AddWall(Simulation.BoardDimensions / 2 - 1, 10, 3, Simulation.BoardDimensions - 20);
+            world.AddWall(Simulation.BoardDimensions / 2 - 1, 10, 3, Simulation.BoardDimensions - 20);
             
             // middle horz
             //world.AddWall(10, Simulation.BoardDimensions / 2 - 1, Simulation.BoardDimensions - 20, 3);
@@ -17,16 +17,13 @@ namespace evolvecli
 
             //world.AddWall(Simulation.BoardDimensions / 3, Simulation.BoardDimensions / 3 + 5, 1, Simulation.BoardDimensions / 3 - 10);
             //world.AddWall((Simulation.BoardDimensions / 3) * 2, Simulation.BoardDimensions / 3 + 5, 1, Simulation.BoardDimensions / 3 - 10);
-
-            
             
             for (int i = 0; i < Simulation.TotalNodes; i++)
             {
                 Node n = new Node(world, Genome.CreateRandom(Simulation.GenesPerGenome));
                 world.AddAtRandom(n);
             }
-
-
+            
             using (Board board = new Board(world))
             {
                 Simulator sim = new Simulator(world, board);
