8e0b81bc07c800091ca3d29c87f422ec6a814276
Robert Horvick <robert.horvick@gmail.com>
2022-03-11 15:03:56 -0500

    programing arguments

--- evolve/Simulation.cs
+++ evolve/Simulation.cs
@@ -4,25 +4,28 @@ namespace evolve
 {
     public static class Simulation
     {
-        public const int TotalNodes = 1000;
-        public const int Generations = 1000;
+        public static int TotalNodes = 1000;
+        public static int Generations = 1000;
         public const int StepsPerGeneration = FPS * 10;
-        public const double SuccessThreshold = 0.95;
-        public const int BoardDimensions = 128;
-        public const int GenesPerGenome = 6;
-        public const int InnerNeurons = 3;
-        public const double MutationChance = 1d / TotalNodes; 
-        public const bool AllowKillers = false;
-        public const bool AllowBullies = true;
-        public const bool AllowDefense = false;
+        public static double SuccessThreshold = 0.95;
+        public static int BoardDimensions = 128;
+        public static int GenesPerGenome = 6;
+        public static int InnerNeurons = 3;
+        public static readonly double MutationChance = 1d / TotalNodes; 
+        public static bool AllowKillers = false;
+        public static bool AllowBullies = true;
+        public static bool AllowDefense = false;
+        public static int BreedingGroundId = 1;
 
-        private const int RenderOnMultiple = 100;
+        public static int RenderOnMultiple = Generations / 10;
         private const int FPS = 30;
 
         public static bool WinThresholdExceeded = false;
 
+        public static bool RenderImages = false;
+
         public static bool RenderFrame =>
-            false
+            RenderImages
                 &&  (CurrentGeneration == 1
                     || WinThresholdExceeded
                     || CurrentGeneration == Generations
@@ -43,15 +46,17 @@ namespace evolve
 
         public static bool InBreedingGrounds(int x, int y)
         {
-            // left side
-            return x < 15;
-            
-            // left side but not the corners
-            // return x < 15 && y > 15 && y < BoardDimensions - 15;
-            
-            // the middle
-            // return x > BoardDimensions / 3 && x < (BoardDimensions / 3) * 2
-            //                               && y > BoardDimensions / 3 && y < (BoardDimensions / 3) * 2;
+            return BreedingGroundId switch
+            {
+                1 => x < 15, // left side
+                2 => x > BoardDimensions - 15, // right
+                3 => y < 15, // top
+                4 => y > BoardDimensions -15, // bottom
+                5 => x < 15 && y > 15 && y < BoardDimensions - 15, // part of left
+                6 => x > BoardDimensions / 3 && x < (BoardDimensions / 3) * 2 // middle
+                         && y > BoardDimensions / 3 && y < (BoardDimensions / 3) * 2,
+                _ => x < 15 // default to left side
+            };
         }
     }
 }
--- evolvecli/Program.cs
+++ evolvecli/Program.cs
@@ -5,28 +5,81 @@ namespace evolvecli
 {
     internal class Program
     {
+        private static string arg(string[] args, string arg, string defaultValue)
+        {
+            foreach (string s in args)
+            {
+                if (s.StartsWith(arg))
+                {
+                    var v = s.Split(new[] { '=' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                    return v[1];
+                }
+            }
+
+            return defaultValue;
+        }
+
+        private static int arg(string[] args, string arg, int defaultValue)
+        {
+            foreach(string s in args)
+            {
+                if(s.StartsWith(arg))
+                {
+                    var v = s.Split(new[] { '=' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                    return int.Parse(v[1]);
+                }
+            }
+
+            return defaultValue;
+        }
+
+        private static bool arg(string[] args, string arg, bool defaultValue)
+        {
+            foreach (string s in args)
+            {
+                if (s.StartsWith(arg))
+                {
+                    var v = s.Split(new[] { '=' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                    return bool.Parse(v[1]);
+                }
+            }
+
+            return defaultValue;
+        }
+
         public static void Main(string[] args)
         {
+            Simulation.TotalNodes = arg(args, "nodes", Simulation.TotalNodes);
+            Simulation.Generations = arg(args, "gens", Simulation.Generations);
+            Simulation.GenesPerGenome = arg(args, "genes", Simulation.GenesPerGenome);
+            Simulation.InnerNeurons = arg(args, "inner", Simulation.InnerNeurons);
+            Simulation.AllowKillers = arg(args, "killers", Simulation.AllowKillers);
+            Simulation.AllowBullies = arg(args, "bullies", Simulation.AllowBullies);
+            Simulation.AllowDefense = arg(args, "defenders", Simulation.AllowDefense);
+            Simulation.RenderImages = arg(args, "render", Simulation.RenderImages);
+            Simulation.BreedingGroundId = arg(args, "grounds", Simulation.BreedingGroundId);
+            var walls = arg(args, "walls", string.Empty);
+
             World world = new World(Simulation.BoardDimensions);
 
-            // middle vert
-            //world.AddWall(Simulation.BoardDimensions / 2 - 1, 10, 3, Simulation.BoardDimensions - 20);
-            
-            // middle horz
-            //world.AddWall(10, Simulation.BoardDimensions / 2 - 1, Simulation.BoardDimensions - 20, 3);
+            switch(walls)
+            {
+                case "cross":
+                    world.AddWall(Simulation.BoardDimensions / 2 - 1, 10, 3, Simulation.BoardDimensions - 20);
+                    world.AddWall(10, Simulation.BoardDimensions / 2 - 1, Simulation.BoardDimensions - 20, 3);
+                    break;
 
+                case "hard":
+                    world.AddWall(Simulation.BoardDimensions / 3, Simulation.BoardDimensions / 3 + 5, 1, Simulation.BoardDimensions / 3 - 10);
+                    world.AddWall((Simulation.BoardDimensions / 3) * 2, Simulation.BoardDimensions / 3 + 5, 1, Simulation.BoardDimensions / 3 - 10);
+                    break;
+            }
 
-            //world.AddWall(Simulation.BoardDimensions / 3, Simulation.BoardDimensions / 3 + 5, 1, Simulation.BoardDimensions / 3 - 10);
-            //world.AddWall((Simulation.BoardDimensions / 3) * 2, Simulation.BoardDimensions / 3 + 5, 1, Simulation.BoardDimensions / 3 - 10);
-
-            
-            
             for (int i = 0; i < Simulation.TotalNodes; i++)
             {
                 Node n = new Node(world, Genome.CreateRandom(Simulation.GenesPerGenome));
                 world.AddAtRandom(n);
             }
-
 
             using (Board board = new Board(world))
             {
